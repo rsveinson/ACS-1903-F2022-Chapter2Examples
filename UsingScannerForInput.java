242f68c6a00f5187b60e88db300b827b415a8133
rsveinson <rsveinson@mts.net>
2023-09-22 14:17:58 -0500

    minot changes

--- UsingScannerForInput.java
+++ UsingScannerForInput.java
@@ -9,13 +9,15 @@ public class UsingScannerForInput
         
         // Declare a scanner object for the keyboard
         Scanner keyboard = new Scanner(System.in);
-        // Prompt the user for a name
         
+        // Prompt the user for a name       
         System.out.println("\n\nEnter your name (no spaces) and press enter");
         name = keyboard.next();
+        
         // Prompt the user for hours worked
         System.out.println("Enter the hours worked (no decimal places) and press enter");
         hoursWorked = keyboard.nextInt();
+        
         // Prompt the user for the rate of pay
         System.out.println("Enter your rate of pay and press enter");
         rateOfPay = keyboard.nextDouble();
